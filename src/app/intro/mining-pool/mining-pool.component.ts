c63b9b68f62b89cc3cb602093446d15ac971b4e4
JessDotJS <jessmanny19@gmail.com>
2017-05-11 09:08:44 -0400

    Template v0.1

---
+++ src/app/intro/mining-pool/mining-pool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mining-pool',
+  templateUrl: './mining-pool.component.html',
+  styleUrls: ['./mining-pool.component.css']
+})
+export class MiningPoolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
